22750bea1951ea9fa017f257c6726dcc93e7ac1f
JaZahn <jcleveng@fas.harvard.edu>
2015-12-02 18:24:47 -0500

    reverting to base

--- logger.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(request, response){
-  var date = (new Date()).toString();
-  console.log(date + " " + response.statusCode + " " + request.method + " " + request.url + " " + request.headers['user-agent']);
-};
